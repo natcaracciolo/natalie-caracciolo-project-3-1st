a9c26bcab79113e7a53cafd2eb8639d1a54a774f
Natalie Caracciolo <nataliecaracciolo@gmail.com>
2019-05-18 11:57:57 -0400

    2nd commit

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 
+$(document).ready(function(){
 
-// // quiz is going to correspond each answer with a point sytem, then create a result based on how many points the user got.
+    const submitButton = document.querySelector(".submit-button");
 
+    // $(`#submit-button`)
+    const test = $('.submit-button');
+    console.log('logging out submit button', submitButton);
+    console.log('logging out test', test[0]);
+    submitButton.addEventListener("click", getScore);
 
-//create function for the submit button that 
-const submitButton = document.querySelector("#submit-button");
+}) 
 
-submitButton.addEventListener("click", getScore);
+    $(".button1").click(function() {
+        $('html,body').animate({
+            scrollTop: $("form").offset().top},
+             'slow');
 
+          
 
-//make function that return oblects with all elements in the HTML that match input. also target the 'my quiz sections'
+});
+
 
 function getScore() {
-    const form = document.querySelector("#my-quiz");
+    const form = document.querySelector("#quiz");
     const inputs = form.querySelectorAll("input");
 
-// give every option a value of 0 (starting point)
+
     let joyDivision = 0;
     let talkingHeads = 0;
     let pointerSisters = 0;
     
     
     let yourType;
-    let winningScore = 1;
+    let winningScore = 0;
     
-
-    //match every expression's value to a 'case', which makes the statments run that are associated with the 'case' and statements in the following case. 
-
     
     Array.prototype.forEach.call(inputs, function(input) {
         if (input.checked) {
-        //switch is the expression
+      
             switch (input.value) {
            // case is matched to the expression
                 case "joyDivision": joyDivision++; break;
@@ -40,29 +47,26 @@ function getScore() {
                 
                 default: 
                 $('.quiz-answer').empty()
-                $('.quiz-answer').append
+                // $('.quiz-answer').append
             }
         }
     });
 
-    
+
     largestScore = joyDivision;
-    yourType = "Disorder by Joy Division";
-    if (joyDivision > largestScore) {
-        yourType = "Once In A Lifetime by Talking Heads";
+    yourType = "Disorder by Joy Division. You must be super fun at parties...";
+    if (talkingHeads > largestScore) {
+        yourType = "Once In A Lifetime by Talking Heads. Stay weird!";
         largestScore = talkingHeads;
     }
     if (pointerSisters > largestScore) {
-        yourType = "I'm So Excited by The Pointer Sisters";
+        yourType = "I'm So Excited by The Pointer Sisters. Show me one person that doesnt like this song.";
         largestScore = pointerSisters;
     }
-
-
-
+  
 
         $('.quiz-answer').empty()
-        $('.quiz-answer').append(`<p>our 80's jam is ${yourType}!</p>`)
-  
+        $('.quiz-answer').append(`<p>Your 80's jam is: ${yourType}</p>`)
     
 
 }
