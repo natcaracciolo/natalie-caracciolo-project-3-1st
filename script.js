316628e8f1bbd8a3e490238f3d04f819679d5b62
Natalie Caracciolo <nataliecaracciolo@gmail.com>
2019-05-20 15:37:20 -0400

    more styles updated

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 $(document).ready(function(){
 
     const submitButton = document.querySelector(".submit-button");
-
-    // $(`#submit-button`)
     const test = $('.submit-button');
     console.log('logging out submit button', submitButton);
     console.log('logging out test', test[0]);
@@ -20,10 +18,10 @@ function createScroller(clickElement, target) {
     });
 }
 
-createScroller('.button1', 'form')
-createScroller('.question1', '.question2')
-createScroller('.question2', '.question3')
-createScroller('.submit-button', '.results')
+    createScroller('.button-1', 'form')
+    createScroller('.question-1', '.question-2')
+    createScroller('.question-2', '.question-3')
+    createScroller('.submit-button', '.results')
 
 function getScore() {
     const form = document.querySelector("#quiz");
@@ -32,53 +30,58 @@ function getScore() {
 
     let joyDivision = 0;
     let talkingHeads = 0;
-    let pointerSisters = 0;
+    let newOrder = 0;
+    let saltPeppa = 0;
     
     
     let yourType;
     let winningScore = 0;
     
-    
+    console.log(inputs)
     Array.prototype.forEach.call(inputs, function(input) {
-        if (input.checked) {
+       
+    if (input.checked) {
       
-            switch (input.value) {
-           // case is matched to the expression
-                case "joyDivision": joyDivision++; break;
-                // this is going to incriment when the value of the expression matches'joy division'
-                case "talkingHeads": talkingHeads++; break;
-                case "pointerSisters": pointerSisters++; break;
+        switch (input.value) {
+            case "joyDivision": joyDivision++; break;
+            case "talkingHeads": talkingHeads++; break;
+            case "newOrder": newOrder++; break;
+            case "saltPeppa": saltPeppa++; break;
+            
                 
-                default: 
-                $('.quiz-answer').empty()
-                // $('.quiz-answer').append
-            }
+            default: 
+            $('.quiz-answer').empty()
+                
         }
-    });
-
-
-    largestScore = joyDivision;
-    yourType = "Disorder by Joy Division. You must be super fun at parties...";
-    if (talkingHeads > largestScore) {
-        yourType = "Once In A Lifetime by Talking Heads. Stay weird!";
-        largestScore = talkingHeads;
     }
-    if (pointerSisters > largestScore) {
-        yourType = "I'm So Excited by The Pointer Sisters. Show me one person that doesnt like this song.";
-        largestScore = pointerSisters;
-    }
-  
+});
 
-        $('.quiz-answer').empty()
-        $('.quiz-answer').append(`<p>Your 80's jam is: ${yourType}</p>`)
+    let largestScore = 0;
+        yourType = "Nothing - because you need to select an option from each question to get your song!";
+
+        if (joyDivision > largestScore) {
+            yourType = "Joy Division, 'Disorder'. You must be super fun at parties....";
+        }
+        if (talkingHeads > largestScore) {
+            yourType = "Talking Heads, 'Once In A Lifetime'. Stay weird!";
+            largestScore = talkingHeads;
+        }
+        if (newOrder > largestScore) {
+            yourType = "New Order, 'Bizarre Love Triangle'. Show me one person that doesn't like this song.";
+            largestScore = newOrder;
+        }
+        if (saltPeppa > largestScore) {
+             yourType = "Salt-N-Peppa, 'Push It'. Congratulations - You are super fly.";
+            largestScore = saltPeppa;
+        }
+
+      
+   
+
+    $('.quiz-answer').empty()
+    $('.quiz-answer').append(`<p>Your 80's jam is: ${yourType}</p>`)
+    
+
+       
     
 }
-
-
-
-
-
-
-
-
-    
